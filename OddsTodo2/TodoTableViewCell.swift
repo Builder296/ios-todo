ae60fc021a8054db5e001bfa9a45aae6b2e55aab
builder <kessirin296@gmail.com>
2022-04-19 15:46:32 +0700

    Add cocoa touch class for table view cell

---
+++ OddsTodo2/TodoTableViewCell.swift
@@ -0,0 +1,23 @@
+//
+//  TodoTableViewCell.swift
+//  OddsTodo2
+//
+//  Created by kessirin muenwongsa on 19/4/2565 BE.
+//
+
+import UIKit
+
+class TodoTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
